524b680e712bd5d8da6941c95afc4ebbadb55634
Hoo <hahaha1321@cau.ac.kr>
2023-02-26 17:31:55 +0900

    feat: Add translation functionality to popup window
    This commit updates the manifest, popup.html, and popup.js files to include translation functionality. The extension now translates the selected text to Japanese and displays the translation in the popup window when the extension's icon is clicked.

--- popup.js
+++ popup.js
@@ -1,1 +1,24 @@
-console.log('Popup opened!');
+function translateText(selectedText) {
+    const sourceLang = "auto";
+    const targetLang = "ja";
+    const translateUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(selectedText)}`;
+  
+    fetch(translateUrl)
+      .then(response => response.json())
+      .then(data => {
+        const translation = data[0][0][0];
+        document.getElementById("translation").innerText = translation;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  
+  chrome.tabs.executeScript({
+    code: "window.getSelection().toString();"
+  }, function (selectedText) {
+    if (selectedText[0]) {
+      translateText(selectedText[0]);
+    }
+  });
+  
