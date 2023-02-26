0c2daacd0e35eaaa27775fd1ca6bffc85659bcc9
Hoo <hahaha1321@cau.ac.kr>
2023-02-26 17:42:56 +0900

    feat: Add kanji extraction to translation functionality
    This commit updates the popup.js file to include a new function called 'extractKanji', which extracts only the kanji characters from the translated Japanese text. The 'translateText' function has been updated to call this new function and display the extracted kanji in the popup window.

--- popup.js
+++ popup.js
@@ -1,4 +1,13 @@
-function translateText(selectedText) {
+function extractKanji(translation) {
+    const kanjiRegex = /[\u4e00-\u9faf]+/g;
+    const kanjiList = translation.match(kanjiRegex);
+    const twoOrMoreKanjiList = kanjiList.filter(kanji => kanji.length >= 2);
+    const twoOrMoreKanji = twoOrMoreKanjiList.join("");
+  
+    return twoOrMoreKanji;
+  }
+  
+  function translateText(selectedText) {
     const sourceLang = "auto";
     const targetLang = "ja";
     const translateUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(selectedText)}`;
@@ -7,7 +16,8 @@ function translateText(selectedText) {
       .then(response => response.json())
       .then(data => {
         const translation = data[0][0][0];
-        document.getElementById("translation").innerText = translation;
+        const kanji = extractKanji(translation);
+        document.getElementById("translation").innerText = kanji;
       })
       .catch(error => {
         console.error(error);
