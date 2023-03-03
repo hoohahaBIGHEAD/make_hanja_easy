e6a839f2aba6fe72f0e6860e04d394335cbcd540
Hoo <hahaha1321@cau.ac.kr>
2023-03-04 00:26:07 +0900

    refactor(content.js): Refactor translateText function
    - Divide translateText into separate functions for better readability and maintainability
    - Use promises to handle asynchronous fetch requests
    - Rename generateOutput to displayTranslation for better naming
    
    This commit refactors the translateText function in popup.js by breaking it down into smaller, more modular functions. The fetchTranslation function handles fetching translations from the API and returns a promise, while extractKanji and lookupKanji functions extract kanji from translations and look up their meanings in the dictionary respectively. The generateOutput function has been renamed to displayTranslation for better naming consistency. The changes improve the readability and maintainability of the code.

--- popup.js
+++ popup.js
@@ -16,62 +16,91 @@ function extractKanji(translation) {
     }
   }
   
-  function translateText(selectedText) {
+  function fetchTranslation(selectedText) {
     const sourceLang = "auto";
     const targetLang = "ja";
     const translateUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(selectedText)}`;
-  
-    fetch(translateUrl)
+    
+    return fetch(translateUrl)
       .then(response => response.json())
       .then(data => {
         const translation = data[0][0][0];
-        const kanjiList = extractKanji(translation);
-        let words = [kanjiList[0]];
-  
-        for (let i = 1; i < kanjiList.length; i++) {
-          const prevKanji = kanjiList[i - 1];
-          const curKanji = kanjiList[i];
-          const prevInDic = prevKanji in hanjaDic;
-          const curInDic = curKanji in hanjaDic;
-          if (prevInDic && curInDic) {
-            words[words.length - 1] += curKanji;
-          } else {
-            words.push(curKanji);
-          }
-        }
-  
-        const output = words.map(w => {
-          const kanjiKors = w.split("").map(k => {
-            const kanjiLookup = hanjaDic[k];
-            if (kanjiLookup) {
-              return kanjiLookup.map(l => l.kor);
-            } else {
-              return [""];
-            }
-          });
-          const kanjiDefs = w.split("").map(k => {
-            const kanjiLookup = hanjaDic[k];
-            if (kanjiLookup) {
-              return kanjiLookup.map(l => l.def);
-            } else {
-              return [""];
-            }
-          });
-
-          const allCombos = cartesianProduct(kanjiKors);
-          const wordWithParens = w + "(" + allCombos.map(c => c.join("")).join(", ") + ")";
-          const wordDefs = cartesianProduct(kanjiDefs).map(combo => {
-            return "[" + combo.join(", ") + "]";
-          }).join(", ");
-          return wordWithParens + ": " + wordDefs;
-        }).join("\n");
-  
-        document.getElementById("translation").innerText = output;
+        return translation;
       })
       .catch(error => {
         console.error(error);
       });
   }
+  
+  function extractKanji(translation) {
+    const kanjiRegex = /[\u4e00-\u9faf]+/g;
+    const kanjiList = translation.match(kanjiRegex);
+    const twoOrMoreKanjiList = kanjiList.filter(kanji => kanji.length >= 2);
+  
+    return twoOrMoreKanjiList;
+  }
+  
+  function lookupKanji(kanji) {
+    if (kanji in hanjaDic) {
+      const definitions = hanjaDic[kanji];
+      const defList = definitions.map(def => def.def + " " + def.kor);
+      return defList.join(", ");
+    } else {
+      return "";
+    }
+  }
+  
+  function generateOutput(words) {
+    const output = words.map(w => {
+      const kanjiKors = w.split("").map(k => {
+        const kanjiLookup = hanjaDic[k];
+        if (kanjiLookup) {
+          return kanjiLookup.map(l => l.kor);
+        } else {
+          return [""];
+        }
+      });
+      const kanjiDefs = w.split("").map(k => {
+        const kanjiLookup = hanjaDic[k];
+        if (kanjiLookup) {
+          return kanjiLookup.map(l => l.def);
+        } else {
+          return [""];
+        }
+      });
+  
+      const allCombos = cartesianProduct(kanjiKors);
+      const wordWithParens = w + "(" + allCombos.map(c => c.join("")).join(", ") + ")";
+      const wordDefs = cartesianProduct(kanjiDefs).map(combo => {
+        return "[" + combo.join(", ") + "]";
+      }).join(", ");
+      return wordWithParens + ": " + wordDefs;
+    }).join("\n");
+  
+    document.getElementById("translation").innerText = output;
+  }
+  
+  function translateText(selectedText) {
+    fetchTranslation(selectedText).then(translation => {
+      const kanjiList = extractKanji(translation);
+      let words = [kanjiList[0]];
+  
+      for (let i = 1; i < kanjiList.length; i++) {
+        const prevKanji = kanjiList[i - 1];
+        const curKanji = kanjiList[i];
+        const prevInDic = prevKanji in hanjaDic;
+        const curInDic = curKanji in hanjaDic;
+        if (prevInDic && curInDic) {
+          words[words.length - 1] += curKanji;
+        } else {
+          words.push(curKanji);
+        }
+      }
+  
+      generateOutput(words);
+    });
+  }
+  
   
   function cartesianProduct(arrays) {
     if (!arrays || !arrays.length) {
